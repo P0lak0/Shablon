83e4c31b931709267a8cc72181377d65cfddb11e
Erwave <116972609+P0lak0@users.noreply.github.com>
2025-01-26 22:54:33 +0300

    Delete modules directory

--- modules/footer.js
+++
@@ -1,5 +0,0 @@
-import Element from "../constructor/Element.js";
-
-const footer = new Element('div', 'footer', 'Footer');
-
-export default footer;
--- modules/header.js
+++
@@ -1,5 +0,0 @@
-import Element from "../constructor/Element.js";
-
-const header = new Element('div', 'header', 'Header');
-
-export default header;
