a4aa3712766c7373e46252f5a8b561b0c4ba6bba
Erwave <vladislavzakharov31@gmail.com>
2025-01-26 22:45:09 +0300

    Two commit

---
+++ PS/main.js
@@ -0,0 +1,7 @@
+import Wrapper from "./constructor/Wrapper.js";
+import content from "./modules/content.js";
+import footer from "./modules/footer.js";
+import header from "./modules/header.js";
+import sidebar from "./modules/sidebar.js";
+
+new Wrapper('#root', [sidebar, header, content, footer]);
